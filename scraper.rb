6a11c549ec14b9288af6dcef18a150e91ca6c9fb
Daniel O'Connor <daniel.oconnor@gmail.com>
2025-12-13 00:28:26 +0000

    Add basic scraper

---
+++ scraper.rb
@@ -0,0 +1,19 @@
+require 'scraperwiki'
+require 'mechanize'
+
+agent = Mechanize.new
+index = Nokogiri::HTML(agent.get("https://www.maribyrnong.vic.gov.au/Building-and-Planning/Advertised-Planning-Applications").body)
+items = []
+index.xpath("//article").each do |article|
+  record = {
+      'council_reference' => article.xpath('a/p[@class="da-application-number small-text"]').text,
+      'address' => article.xpath('a/p[@class="list-item-address"]').text,
+      'description' => article.xpath("a/p[3]").text,
+      'info_url' => article.xpath("a/@href"),
+      'date_scraped' => Date.today.to_s,
+      # 'date_received' => # Not available
+    }
+
+  puts "Storing #{record['council_reference']} - #{record['address']}"
+  ScraperWiki.save_sqlite(['council_reference'], record)
+end
