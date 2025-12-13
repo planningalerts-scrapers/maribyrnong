80a0435e1352ae987854b22e0f1997e32c6cd8aa
Daniel O'Connor <daniel.oconnor@gmail.com>
2025-12-13 00:45:17 +0000

    Cast to text

--- scraper.rb
+++ scraper.rb
@@ -9,7 +9,7 @@ index.xpath("//article").each do |article|
       'council_reference' => article.xpath('a/p[@class="da-application-number small-text"]').text,
       'address' => article.xpath('a/p[@class="list-item-address"]').text,
       'description' => article.xpath("a/p[3]").text,
-      'info_url' => article.xpath("a/@href"),
+      'info_url' => article.xpath("a/@href").text,
       'date_scraped' => Date.today.to_s,
       # 'date_received' => # Not available
     }
